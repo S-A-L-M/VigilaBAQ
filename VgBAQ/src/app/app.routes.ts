679892ac6d6e725933f70d9842e1b9546fcee17c
Santiago Lozano <santiago.lozano@domotes.local>
2025-05-20 17:59:15 -0500

    ADD: Auth Module

--- VgBAQ/src/app/app.routes.ts
+++ VgBAQ/src/app/app.routes.ts
@@ -3,6 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    loadChildren: () => import('./auth/auth.route').then((m) => m.routes),
+  },
+  {
+    path: 'notfound',
+    loadComponent: () => import('./shared/notfound/notfound.page').then( m => m.NotfoundPage)
+  },
+  {
+    path: 'button',
+    loadComponent: () => import('./shared/button/button.page').then( m => m.ButtonPage)
+  },
+  {
+    path: 'validators-form',
+    loadComponent: () => import('./shared/validators-form/validators-form.page').then( m => m.ValidatorsFormPage)
   },
 ];
