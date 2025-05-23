ddbe8cbc0dac067aa6aa01fba9ff4d266514e5be
Santiago Lozano <santiago.lozano@domotes.local>
2025-05-23 16:06:21 -0500

    ADD: Icons Import and Clean: Test Componentes files

--- src/app/main/pages/home/home.page.ts
+++ src/app/main/pages/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardContent, IonButton, IonCol, IonCard, IonIcon, IonCardHeader, IonCardTitle, IonItem, IonLabel, IonList, IonRow, IonAvatar, IonProgressBar, IonGrid, IonNote, IonCardSubtitle, IonText } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoIonic, homeOutline, notificationsOutline, settingsOutline, searchOutline, heartOutline, personOutline, shieldOutline } from 'ionicons/icons';
+import { IonContent, IonCardContent, IonButton, IonCol, IonCard, IonIcon, IonCardHeader, IonCardTitle, IonItem, IonLabel, IonList, IonRow, IonAvatar, IonProgressBar, IonGrid, IonNote, IonCardSubtitle, IonText } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +14,9 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardContent, IonButton,
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({homeOutline,notificationsOutline,settingsOutline,shieldOutline,searchOutline,heartOutline,personOutline,logoIonic});  
+  }
 
   ngOnInit() {
   }
